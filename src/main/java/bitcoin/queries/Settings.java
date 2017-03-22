65010d443945fa8f434cb38530bd6b43e8113cfa
LivioPompianu <pompianu.livio@gmail.com>
2017-03-22 11:31:52 +0000

    Minors

--- src/main/java/bitcoin/queries/Settings.java
+++ src/main/java/bitcoin/queries/Settings.java
@@ -1,5 +1,5 @@
 package bitcoin.queries;
 
 public class Settings {
-    public static final String BLOCKCHAIN_PATH = "D:\\Blockchain\\Bitcoin\\blocks\\";
+    public static final String BLOCKCHAIN_PATH = "D:\\Blockchain\\Bitcoin\\blocks\\"; // Set your local path
 }
